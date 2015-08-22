8c30b59826b8545ace8eead1e51fc7827a8f9be7
Chris Blankenship <chris@dscoduc.com>
2015-08-21 20:27:04 -0700

    Corrected Anonymous User issue

--- PowerShellOnlineSigningService/Main.Master.cs
+++ PowerShellOnlineSigningService/Main.Master.cs
@@ -17,15 +17,18 @@ namespace PowerShellOnlineSigningService
 
         private void displaySessionInfo()
         {
+            string IIS_Auth_Name = string.Empty;
+            string samAccountName = "Anonymous User";
+
             if (HttpContext.Current.User.Identity.IsAuthenticated)
             {
-                string IIS_Auth_Name = HttpContext.Current.User.Identity.Name ?? "Anonymous";
-                string samAccountName = IIS_Auth_Name.Remove(0, IIS_Auth_Name.LastIndexOf(@"\") + 1);
-
-                HtmlGenericControl userInfo = (HtmlGenericControl)Page.Master.FindControl("userInfo");
-                if (userInfo != null)
-                    userInfo.InnerText = string.Format("Welcome {0}", samAccountName);
+                IIS_Auth_Name = HttpContext.Current.User.Identity.Name;
+                samAccountName = IIS_Auth_Name.Remove(0, IIS_Auth_Name.LastIndexOf(@"\") + 1);
             }
+
+            HtmlGenericControl userInfo = (HtmlGenericControl)Page.Master.FindControl("userInfo");
+            if (userInfo != null)
+                userInfo.InnerText = string.Format("Welcome {0}", samAccountName);
 
             HtmlGenericControl serverInfo = (HtmlGenericControl)Page.Master.FindControl("serverInfo");
             if (serverInfo != null)
